747902be1d2d9707511dc9d3a11b4b983f126c47
Akari Ishikawa <ra163282@almeida.lab.ic.unicamp.br>
2018-08-28 16:47:29 -0300

    testing gradient descents

--- exp2_gradientdescents.py
+++ exp2_gradientdescents.py
@@ -28,36 +28,38 @@ def main():
     ax.set_ylabel('Custo')
     ax.set_xlabel('Iterações')
     cores = ['C0', 'C1', 'C2']
-
+    gradients = ["-sgd=0", "-sgd=1"]
 
     train_features, valid_features, train_labels, valid_labels = get_data(1)
+    for g in range(0,len(gradients)):
+        for a in range(0,len(alpha)):
+            prog=[]
+            prog.append("./linearRegressionFlex")
+            prog.append("-a="+str(alpha[a]))
+            prog.append("-i="+str(iteracoes))
+            prog.append("-dvl=0")
+            prog.append(gradients[g])
+            prog.append("-async=1")
+            print(prog)
+            #prog.append("-vr=0")
+            #Executes the call for C code
+            call(prog)
 
-    for a in range(0,len(alpha)):
-        prog=[]
-        prog.append("./linearRegressionFlex")
-        prog.append("-a="+str(alpha[a]))
-        prog.append("-i="+str(iteracoes))
-        prog.append("-dvl=0")
-        prog.append("-sgd=1")
-        prog.append("-async=1")
-        print(prog)
-        #prog.append("-vr=0")
-        #Executes the call for C code
-        call(prog)
+            #train_lr(theta, train_features, train_labels, iterations, alpha)
+            costs = shape_csv('costs.csv')
+            theta = shape_csv('theta.csv')
+            #predictions = shape_csv('predictions.csv')
 
-        #train_lr(theta, train_features, train_labels, iterations, alpha)
-        costs = shape_csv('costs.csv')
-        theta = shape_csv('theta.csv')
-        #predictions = shape_csv('predictions.csv')
-
-        #Plotting
-        if not np.isfinite(costs[0]).all(): 
-            continue
-        else:
-            ax.plot(range(0,iteracoes), costs[0], cores[a], label='Norm ' + str(alpha[a]), linestyle='--')
-            
+            #Plotting
+            if not np.isfinite(costs[0]).all(): 
+                continue
+            elif g==0:
+                ax.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='Batch ' + str(alpha[a]), linestyle='--')
+            else:
+                ax.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='SGD ' + str(alpha[a]), linestyle='-')
+                
     ax.legend()
-    plt.savefig('iters_sgd'+str(iteracoes)+'.png')
+    plt.savefig('gradients_'+str(iteracoes)+'.png')
     fig.show()
 
 if __name__ == "__main__":
