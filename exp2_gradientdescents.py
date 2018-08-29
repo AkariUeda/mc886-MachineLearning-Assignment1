d9751b19b80de0763f4b0f9bee96b97139eea31c
Akari Ishikawa <ra163282@almeida.lab.ic.unicamp.br>
2018-08-28 21:53:04 -0300

    more experiments

--- exp2_gradientdescents.py
+++ exp2_gradientdescents.py
@@ -19,15 +19,20 @@ def shape_csv(name):
 def main():
 
     # Adjusting training parameters
-    iteracoes = 5000
+    iteracoes = 50000
     alpha = [0.2, 0.002, 0.0002]
    
     #Plot settings
     matplotlib.style.use('seaborn')
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.set_ylabel('Custo')
-    ax.set_xlabel('Iterações')
-    cores = ['C0', 'C1', 'C2']
+    fig_train, train_plot = plt.subplots(figsize=(10, 5))
+    train_plot.set_ylabel('Cost')
+    train_plot.set_xlabel('Iterations')
+
+    fig_valid, valid_plot = plt.subplots(figsize=(10, 5))
+    valid_plot.set_ylabel('Cost')
+    valid_plot.set_xlabel('Iterations')
+    cores = ['tab:blue',  'tab:green', 'tab:red', 'tab:orange', 'tab:purple', 'tab:brown']
+
     gradients = ["-sgd=0", "-sgd=1", "-mb=1"]
 
     train_features, valid_features, train_labels, valid_labels = get_data(1)
@@ -37,7 +42,7 @@ def main():
             prog.append("./linearRegressionFlex")
             prog.append("-a="+str(alpha[a]))
             prog.append("-i="+str(iteracoes))
-            prog.append("-dvl=0")
+            prog.append("-dvl=1")
             prog.append(gradients[g])
             prog.append("-async=1")
             prog.append("-vr=0")
@@ -48,22 +53,32 @@ def main():
             #train_lr(theta, train_features, train_labels, iterations, alpha)
             costs = shape_csv('costs.csv')
             theta = shape_csv('theta.csv')
-            #predictions = shape_csv('predictions.csv')
+            predictions = shape_csv('predictCosts.csv')
 
             #Plotting
             if not np.isfinite(costs[0]).all(): 
                 continue
             elif g==0:
-                ax.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='Batch ' + str(alpha[a]), linestyle='--')
+                train_plot.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='Batch ' + str(alpha[a]), linestyle='--')
             elif g==1:
-                ax.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='SGD ' + str(alpha[a]), linestyle='-')
+                train_plot.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='SGD ' + str(alpha[a]), linestyle='-')
             elif g==2:
-                ax.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='MiniBatch ' + str(alpha[a]), linestyle=':')
-                
-    ax.legend()
-    plt.savefig('gradients_'+str(iteracoes)+'.png')
-    fig.show()
-
+                train_plot.plot(range(100,iteracoes), costs[0, 100:], cores[a], label='MiniBatch ' + str(alpha[a]), linestyle=':')
+               
+            if not np.isfinite(predictions[0]).all(): 
+                continue
+            elif g==0:
+                valid_plot.plot(range(100,iteracoes), predictions[0, 100:], cores[a], label='Batch ' + str(alpha[a]), linestyle='--')
+            elif g==1:
+                valid_plot.plot(range(100,iteracoes), predictions[0, 100:], cores[a], label='SGD ' + str(alpha[a]), linestyle='-')
+            elif g==2:
+                valid_plot.plot(range(100,iteracoes), predictions[0, 100:], cores[a], label='MiniBatch ' + str(alpha[a]), linestyle=':')
+    train_plot.legend()
+    valid_plot.legend()
+    fig_train.show()
+    fig_train.savefig('train_gd_'+str(iteracoes)+'.png')
+    fig_valid.show()
+    fig_valid.savefig('valid_gd_'+str(iteracoes)+'.png')
 if __name__ == "__main__":
     main()
 
