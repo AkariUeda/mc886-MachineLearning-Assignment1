8c4e5ad38858d89f8386035acc3b5cc1e08bafcc
Akari Ishikawa <ra163282@silva.lab.ic.unicamp.br>
2018-09-04 18:00:03 -0300

    reorganizing the directories

--- experiments/exp8_sklearn.py
+++ experiments/exp8_sklearn.py
@@ -29,21 +29,26 @@ def main():
 
     cores = ['tab:blue', 'tab:orange']
 
-    train_features = shape_csv('train_features.csv')
-    train_labels = shape_csv('train_labels.csv')
-    valid_features = shape_csv('valid_features.csv')
-    valid_labels = shape_csv('valid_labels.csv')
+    train_features = shape_csv('csv/train_features.csv')
+    train_labels = shape_csv('csv/train_labels.csv')
+    valid_features = shape_csv('csv/valid_features.csv')
+    valid_labels = shape_csv('csv/valid_labels.csv')
+    test_features = shape_csv('csv/test_features.csv')
+    test_labels = shape_csv('csv/test_labels.csv') 
 
+    train_features = np.concatenate((train_features, valid_features), axis=0)
+    train_labels = np.concatenate((train_labels, valid_labels), axis=0)
+ 
     model = linear_model.SGDRegressor(loss='squared_loss', alpha=0.0001, learning_rate='constant')
     model.fit(train_features, train_labels)
 
-    valid_pred = model.predict(valid_features)
-    cost = metrics.mean_squared_error(valid_labels, valid_pred)
+    test_pred = model.predict(test_features)
+    cost = metrics.mean_squared_error(test_labels, test_pred)
     print("Custo predito: "+str(cost))
-    valid_labels, valid_pred = zip(*sorted(zip(valid_labels, valid_pred)))
+    test_labels, test_pred = zip(*sorted(zip(test_labels, test_pred)))
 
-    pred.plot(range(0, len(valid_pred)),valid_pred, 'b.', label="Predicted")
-    pred.plot(range(0, len(valid_labels)), valid_labels, 'r.', label="Target")
+    pred.plot(range(0, len(test_pred)),test_pred, 'b.', label="Predicted")
+    pred.plot(range(0, len(test_labels)), test_labels, 'r.', label="Target")
     pred.legend()
     fig_pred.show()
     fig_pred.savefig('sklearn.png')
