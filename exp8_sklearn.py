d5d46970b4199a5760dd623fbebd4059bc406b85
Akari Ishikawa <ra163282@silva.lab.ic.unicamp.br>
2018-09-04 14:41:39 -0300

    finishing experiments

--- exp8_sklearn.py
+++ exp8_sklearn.py
@@ -17,21 +17,9 @@ def shape_csv(name):
     return file    
 
 def main():
-
-    # Adjusting training parameters
-    iteracoes = int(sys.argv[1])
-    alpha =float(sys.argv[2])
-    gradient = sys.argv[3]
     
     #Plot settings
     matplotlib.style.use('seaborn')
-
-    fig = plt.figure()
-    train_plot = fig.add_subplot(2,1,1)
-    train_plot.set_ylabel('Training cost')
-    valid_plot = fig.add_subplot(2,1,2)
-    valid_plot.set_ylabel('Validation cost')
-    valid_plot.set_xlabel('Iterations')
 
     fig_pred = plt.figure()
     pred = fig_pred.add_subplot(1,1,1)
@@ -45,24 +33,19 @@ def main():
     valid_features = shape_csv('valid_features.csv')
     valid_labels = shape_csv('valid_labels.csv')
 
-    model = linear_model.SGDRegressor()
+    model = linear_model.SGDRegressor(loss='squared_loss', alpha=0.02, learning_rate='constant')
     model.fit(train_features, train_labels)
 
     valid_pred = model.predict(valid_features)
-
-
 
     valid_labels, valid_pred = zip(*sorted(zip(valid_labels, valid_pred)))
 
     pred.plot(range(0, len(valid_pred)),valid_pred, 'b.', label="Predicted")
     pred.plot(range(0, len(valid_labels)), valid_labels, 'r.', label="Target")
     pred.legend()
-    train_plot.legend()
-    valid_plot.legend()
     fig_pred.show()
-    fig_pred.savefig('prediction_'+str(alpha)+"_"+gradient+"_"+str(iteracoes)+'.png')
-    fig.show()
-    fig.savefig('training_'+str(alpha)+"_"+gradient+"_"+str(iteracoes)+'.png')
+    fig_pred.savefig('sklearn.png')
+
 
 if __name__ == "__main__":
     main()
